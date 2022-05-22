3012303d583683dd5cc9ca501bcdaed1131419bf
Aleks-22-05-2022 <leporskayasasha@mail.ru>
2022-05-22 17:03:16 +0300

    Загрузка dostavka.php, o_kompanii.php, partners.php

--- partners.php
+++ partners.php
@@ -1,28 +1,36 @@
+<!-- поиск -->
+
+<?php  
+if ($_GET['result'] != ''){
+	$query_search = $_GET['result'];
+	$query_search = trim($query_search); 
+    $query_search = mysql_real_escape_string($query_search);
+    $query_search =htmlspecialchars($query_search);?>
+	<a href="search.php?result=<?php echo $query_search;?>"></a>
+	<?php
+	
+}
+?>
 <!DOCTYPE html>
 <html lang="ru">
     <head>
         <meta charset="UTF-8">
         <title>Читай</title>
         <link rel="stylesheet" href="style.css">
-		<script defer src="/examples/libs/chief-slider/chief-slider.min.js"></script>
-  <script>
-    document.addEventListener('DOMContentLoaded', function () {
-      const slider = new ChiefSlider('.slider', {
-        loop: false
-      });
-    });
-  </script>
+
     </head>
     <body>
         <header>
-            <a href="#"><img src="photo/logoza_ru.png" class="logo"></a>
-            <input type="text" placeholder="Поиск..." class="search"> <!--Нужно написать поиск по странице-->
-            <input type="submit" value="&#128269;" class="searchSub">
+            <a href="index.php"><img src="photo/logoza_ru.png" class="logo"></a>
+			<form action="search.php?result=<?php echo $query_search;?>">
+				<input type="text" placeholder="Поиск..." class="search"> <!--Нужно написать поиск по странице-->
+				<input type="submit" value="&#128269;" class="searchSub">
+			</form>
             <img src="photo/cabinet.png" class="login"> 
             <a href="" class="log">Войти</a>
             <div class="imgblock">
                 <a href=""><img src="photo/Screenshot_7-removebg-preview.png" alt="Snow"></a>
-                <span>0<?php echo $num; ?></span> <!-- Нужно потом считать количество товаров в корзине -->
+                <span><?php echo $num; ?></span> <!-- Нужно потом считать количество товаров в корзине -->
             </div>
         </header>
         <nav class="menu">
@@ -79,7 +87,10 @@
 					<img width="330px" height="auto" src="img/img-5.jpg" alt="" />
 					<span class="slide-title">СБЕР МАРКЕТ</span>
 				</div>
-				
+				<div class="slide-item">
+					<img width="280px" height="auto" src="img/img-6.jpg" alt="" />
+					<span class="slide-title">PickPoint</span>
+				</div>
 				
 			</div>
 			<div class="clear"></div>
